dc3bf7f963f00e45f36fbf6a8da818a049fcac32
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-07-29 20:36:26 +0300

    wmf menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,30 @@ import React, { useEffect, useState, Suspense } from "react";
 import RemotePage from "./PageRemote";
 import { loadRemoteComponent } from "../loadRemoteComponent";
 
+const LazyMenu = React.lazy(() => import("host_app/Menu"));
+
 const App = () => {
-  const [MenuComponent, setMenuComponent] = useState(null);
+  const [MenuComponent, setMenuComponent] = useState<React.ComponentType | null>(null);
+  const [useLazy, setUseLazy] = useState(true); // Флаг для переключения способа
 
   useEffect(() => {
-    loadRemoteComponent({ remoteName: "host_app", moduleName: "./Menu" })
-      .then((mod) => setMenuComponent(() => mod.default))
-      .catch((err) => console.error("Ошибка загрузки Menu:", err));
-  }, []);
+    if (!useLazy) {
+      loadRemoteComponent({ remoteName: "host_app", moduleName: "./Menu" })
+        .then((mod) => setMenuComponent(() => mod.default))
+        .catch((err) => console.error("Ошибка загрузки Menu:", err));
+    }
+  }, [useLazy]);
 
-  
   return (
     <>
+      <button onClick={() => setUseLazy((prev) => !prev)}>
+        Переключить способ загрузки Menu
+      </button>
+
       <Suspense fallback={<div>Загрузка меню...</div>}>
-        {MenuComponent ? <MenuComponent /> : null}
+        {useLazy ? <LazyMenu /> : MenuComponent ? <MenuComponent /> : null}
       </Suspense>
+
       <RemotePage />
     </>
   );
