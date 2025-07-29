5ca19698fd3d21dbd27ad487fe48ced6b63a16be
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-07-29 15:29:32 +0300

    vercel

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import RemotePage from "./components/RemotePage";
+import RemotePage from "./components/remotePage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<RemotePage />);
