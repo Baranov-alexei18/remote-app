79b90b419979e884953e5c2a8609fa650d680558
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-07-29 17:51:01 +0300

    change link tag

--- src/components/PageRemote.jsx
+++ src/components/PageRemote.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageRemote = () => (
   <>
     <div>Это remote-приложение, загруженное по следующей ссылке</div>
-    <Link href='https://remote-app-roan.vercel.app/'>https://remote-app-roan.vercel.app/</Link>
+    <a href='https://remote-app-roan.vercel.app/'>https://remote-app-roan.vercel.app/</a>
   </>
 );
 
