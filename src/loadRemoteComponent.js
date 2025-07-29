5413e7df19f2ca79e535f76c32f69322af6e2a0e
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-07-29 20:32:38 +0300

    check 11

--- src/loadRemoteComponent.js
+++ src/loadRemoteComponent.js
@@ -8,8 +8,9 @@ export const loadRemoteComponent = async ({ remoteName, moduleName }) => {
   await __webpack_init_sharing__('default');
   await container.init(__webpack_share_scopes__.default);
 
-  // 3. Получи модуль
   const factory = await container.get(moduleName);
   const Module = factory();
+  console.log('Module');
+  console.log(Module);
   return Module;
 };
