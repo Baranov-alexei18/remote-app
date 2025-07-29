36882acf9456df56fb79bee4b7e4bc418bca4f17
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-07-29 17:49:08 +0300

    check 11

--- src/components/PageRemote.jsx
+++ src/components/PageRemote.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const PageRemote = () => (
   <>
-    <div>Это remote-приложение</div>
+    <div>Это remote-приложение, загруженное по следующей ссылке</div>
+    <Link href='https://remote-app-roan.vercel.app/'>https://remote-app-roan.vercel.app/</Link>
   </>
 );
 
