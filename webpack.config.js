e68ce2fe8dc89f99f7f8d48c1878188a876003b8
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-07-29 16:36:51 +0300

    check 2

--- webpack.config.js
+++ webpack.config.js
@@ -39,12 +39,10 @@ new ModuleFederationPlugin({
         react: {
           singleton: true,
           requiredVersion: '^18.2.0',
-          import: false
         },
         'react-dom': {
           singleton: true,
           requiredVersion: '^18.2.0',
-          import: false
         },
       },
     }),
