c9117cd2ef0af26f7faf88c9515f0954dd876b3d
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-07-29 20:55:54 +0300

    wmf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState, Suspense } from "react";
 import RemotePage from "./PageRemote";
 import { loadRemoteComponent } from "../loadRemoteComponent";
-
+import { BrowserRouter } from 'react-router-dom';
 const LazyMenu = React.lazy(() => import("host_app/Menu"));
 
 
 const App = () => {
   // const [MenuComponent, setMenuComponent] = useState<React.ComponentType | null>(null);
-  // const [useLazy, setUseLazy] = useState(true); // Флаг для переключения способа
+  const [useLazy, setUseLazy] = useState(true); // Флаг для переключения способа
 
   // useEffect(() => {
   //   if (!useLazy) {
@@ -18,20 +18,20 @@ const App = () => {
   // }, [useLazy]);
 
   return (
-    <>
+    <BrowserRouter>
       <button onClick={() => setUseLazy((prev) => !prev)}>
         Переключить способ загрузки Menu
       </button>
 
       <Suspense fallback={<div>Загрузка меню...</div>}>
-        {LazyMenu}
+        {useLazy ? LazyMenu : 'попробуем загрузить еще раз'}
       </Suspense>
 
       <Suspense fallback={<div>Загрузка меню...</div>}>
         <LazyMenu />
       </Suspense>
       <RemotePage />
-    </>
+    </BrowserRouter>
   );
 };
 
