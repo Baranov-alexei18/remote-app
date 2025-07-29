5413170b67c1c107eac4f8e3c944312f0bc1fae2
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2025-07-29 15:39:03 +0300

    remote check wmf

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import RemotePage from "./components/remotePage";
+import RemotePage from "./components/RemotePage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<RemotePage />);
